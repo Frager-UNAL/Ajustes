c236ee1eb91c71562fd85bc76e3746759360d27e
NicolasDelgado <nicolas.felipedelgado@gmail.com>
2020-10-07 18:40:15 -0500

    Ajustes

---
+++ Controllers/AjustesControlador.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ajustes_Fragen.Modelos;
+using Ajustes_Fragen.Servicios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+
+namespace Ajustes_Fragen.Controllers
+{
+    [Route("ajustes")]
+    [ApiController]
+    public class AjustesControlador : ControllerBase
+    {
+        private readonly AjustesServicio ajustesS;
+
+        public AjustesControlador(AjustesServicio Aj)
+        {
+            ajustesS = Aj;
+        }
+
+        [HttpGet]
+        public List<String> Get()
+        {
+            List<String> listString = new List<string>();
+            var listaAjustes = ajustesS.Get();
+            foreach(Ajustes ajuste in listaAjustes)
+            {
+                listString.Add(ajuste.ToJson());
+            }
+            return listString;
+        }
+
+        [HttpGet("{id}")]
+        public String Get(string id)
+        {
+            var _ajustes = ajustesS.Get(id);
+
+            if (_ajustes == null)
+            {
+                return null;
+            }
+
+            return _ajustes.ToJson();
+        }
+
+        [HttpPost]
+        public String Create([FromBody] Ajustes _ajustes)
+        {
+            try
+            {
+                return ajustesS.Create(_ajustes);
+            }
+            catch (Exception ex)
+            { 
+                return ex.ToString();
+            }
+            
+        }
+
+        [HttpPut("{id}")]
+        public String PUT(string id, [FromBody] Ajustes value)
+        {
+            var _ajustes = ajustesS.Get(id);
+
+            if (_ajustes == null)
+            {
+                return null;
+            }
+
+            return ajustesS.Update(id, value);
+        }
+
+        [HttpDelete("{id}")]
+        public String Delete(string id)
+        {
+            var _ajustes = ajustesS.Get(id);
+
+            if (_ajustes == null)
+            {
+                return null;
+            }
+
+            return ajustesS.Remove(_ajustes);
+        }
+    }
+}
---
+++ Servicios/AjustesServicio.cs
@@ -0,0 +1,65 @@
+﻿using Ajustes_Fragen.Modelos;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Ajustes_Fragen.Servicios
+{
+    public class AjustesServicio
+    {
+        private IMongoCollection<Ajustes> _ajustes;
+
+        public AjustesServicio(IAjustesDatabaseSettings settings)
+        {
+            
+            var client = new MongoClient("mongodb+srv://admin:Nicolas2020@ajustescloster.ajwc8.mongodb.net/AjustesDB?retryWrites=true&w=majority");
+            var database = client.GetDatabase("AjustesDB");
+
+            _ajustes = database.GetCollection<Ajustes>(settings.AjustesCollectionName);
+        }
+
+        public List<Ajustes> Get()
+        {
+            var lista = _ajustes.Find(s => s.Id != null).ToList(); ;
+            return lista; 
+        }
+
+        public Ajustes Get(string id)
+        {
+            Ajustes document = _ajustes.Find(ajs => ajs.Id == id).FirstOrDefault();
+            //Console.WriteLine(document.ToString());
+            return document;
+            
+        }
+
+        public String Create(Ajustes ajuste)
+        {
+            _ajustes.InsertOne(ajuste);
+            return ajuste.ToJson();
+        }
+
+        public String Update(string id, Ajustes ajus)
+        {
+            ajus.Id = id;
+            _ajustes.ReplaceOne(aj => aj.Id == id, ajus);
+            return ajus.ToJson();
+        }
+
+        public String Remove(Ajustes ajus)
+        {
+            string _id = ajus.Id;
+            _ajustes.DeleteOne(aj => aj.Id == ajus.Id);
+            return _id;
+        }
+
+        public String Remove(string id)
+        {
+            _ajustes.DeleteOne(aj => aj.Id == id);
+            return id;
+        }
+    }
+}
